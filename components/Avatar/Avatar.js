4dff28074e9bf86988a8601a4006eb040ca2e482
Pilip.Shchasny <c-pshchasny@neogov.net>
2022-01-03 15:02:18 +0300

    pshchasny-added-btns-group

--- components/Avatar/Avatar.js
+++ components/Avatar/Avatar.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Avatar = ({url}) => {
   return (
     <img className="h-10 rounded-full
+       animate-bounce
        cursor-pointer
        transition duration-150
        transform hover:scale-110"
