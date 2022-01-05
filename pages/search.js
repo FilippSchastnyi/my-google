b794aa27a533995932d1defdf46e4067a72ff5c7
Pilip.Shchasny <c-pshchasny@neogov.net>
2022-01-05 18:19:09 +0300

    pshchasny-revert

--- pages/search.js
+++ pages/search.js
@@ -1,21 +1,42 @@
 import Head from "next/head";
 import Header from "../components/Header/Header";
-const Search = () => {
+import {API_KEY, CONTEXT_KEY} from "../keys";
+import Response from "../Response"
+import {useRouter} from "next/router";
+import SearchResults from "../components/SearchResults/SearchResults";
+
+function Search({ results }) {
+
+  const router = useRouter();
+
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google2.0 Search</title>
+        <link rel="icon" href="" />
       </Head>
-
-      <Header/>
-      {/*searchResult*/}
+      {/*  Header  */}
+      <Header />
+      {/* Search Results */}
+      <SearchResults results={results} />
     </div>
-  );
-};
+  )
+}
 
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData = false;
-  
+  const useDummy = false;
+
+  const startPagination = context.query.start || '0'
+
+  const data = useDummy
+    ? Response
+    : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startPagination}`).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    }
+  }
 }
